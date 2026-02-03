13d25d500cd1aaf60a6e20c8d114042bf98efddc
Fire <17692105+FireMario211@users.noreply.github.com>
2026-02-03 18:08:57 -0500

    Merge pull request #9 from FireMario211/main
    support for v5

--- include/Events.hpp
+++ include/Events.hpp
@@ -1,17 +1,8 @@
 #pragma once 
-#include <Geode/loader/Event.hpp>
 #include "layers/SogLayer.hpp"
 
 namespace soggy_mod {
-
-class OnSogLayer : public geode::Event {
-protected:
-    SogLayer* m_layer;
-    int m_rand;
-public:
-    OnSogLayer(SogLayer* layer, int rand) : m_layer(layer), m_rand(rand) {};
-    SogLayer* getLayer() const { return m_layer; };
-    int getRandom() const { return m_rand; };
-};
-
+    struct OnSogLayer final : geode::SimpleEvent<OnSogLayer, SogLayer*> {
+        using SimpleEvent::SimpleEvent;
+    };
 }
